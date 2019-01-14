fb03767def13558bcc408f6e5f9fcea48d95ca25
Clara Munro and Theary Im <theary@>
2019-01-14 14:27:31 -0800

    added more js, reverse functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,30 @@
 
 var sentence = prompt("Enter a sentence:");
 alert (remove());
+alert (reverse());
+alert (newThree());
 
 
 function remove () {
   var removeFirst = sentence.slice(0,1);
   var removeLast = sentence.slice(sentence.length-1, sentence.length);
-
-  var characters = `${removeFirst} ${removeLast}`.toUpperCase();
+  var characters = `${removeFirst}${removeLast}`.toUpperCase();
   return characters;
 }
 
 function reverse () {
   var blah = remove();
-
   blah = blah.split(""); //convert 'blahing' to array
   blah = blah.reverse(); //reverse 'blahing' order
   blah = blah.join(""); //then join the reverse order values together
-  alert(blah);
-  console.log(blah);
   return blah;
 }
 
-
 function newThree () {
   var clara = remove();
-  var car = reverse();
-  console.log(clara, car);
-  return clara, car;
+  var car = reverse(clara);
+  var string = `${sentence}${car}`;
+  return string;
 }
 
-//
-// remove();
-// reverse();
 newThree();
